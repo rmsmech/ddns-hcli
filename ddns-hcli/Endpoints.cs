7392bb47e975e297daa8802ba258444958428a65
Senguttuvan Mahalingam <rmsmech@gmail.com>
2024-11-17 22:37:15 +0400

    json changes

---
+++ ddns-hcli/Endpoints.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ddns_hcli {
+    internal class Endpoints {
+        public const string GET_ALL_RECORDS = $@"https://api.cloudflare.com/client/v4/zones/@ZONE_ID/dns_records";
+    }
+}
--- ddns-hcli/Worker.cs
+++ ddns-hcli/Worker.cs
@@ -1,27 +1,39 @@
 using Haley.Utils;
 using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace ddns_hcli {
     public class Worker : BackgroundService {
         private readonly ILogger<Worker> _logger;
-        const string _confName = "ddns.conf";
+        const string _cfgFileName = "ddns.conf";
+        string _cfgFilePath = string.Empty;
+        JsonNode _cfg = null;
         public Worker(ILogger<Worker> logger) {
-            //Change logger to Haley logger, so that we can start dumping the logs to a file.
-            _logger = logger;
-            var conf = Path.Combine(AssemblyUtils.GetBaseDirectory(), _confName);
-            _logger.LogInformation("Configration Search path " + conf);
-            if (!File.Exists(conf)) {
-                //Then try to export the file from embedded data.
-                _logger.LogInformation("Configuration doesn't exists. Exporting the default file.");
-                //var names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
-                var res = ResourceUtils.GetEmbeddedResource($@"ddns_hcli.{_confName}", Assembly.GetExecutingAssembly());
-                using (var fs = new FileStream(conf, FileMode.Create)) {
-                    fs.Write(res);
-                    fs.Close();
+            try {
+                //Change logger to Haley logger, so that we can start dumping the logs to a file.
+                _logger = logger;
+                //Should we even expect the config also to be present in same folder as the main application? What if the user deletes some main dll by mistake???
+                //TODO: Change the location of the conf file to some other location.
+                _cfgFilePath = Path.Combine(AssemblyUtils.GetBaseDirectory(), _cfgFileName); //to optimize later
+                _logger.LogInformation("Configration Search path " + _cfgFilePath);
+                if (!File.Exists(_cfgFilePath)) {
+                    //Then try to export the file from embedded data.
+                    _logger.LogInformation("Configuration doesn't exists. Exporting the default file.");
+                    //var names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+                    var res = ResourceUtils.GetEmbeddedResource($@"ddns_hcli.{_cfgFileName}", Assembly.GetExecutingAssembly());
+                    using (var fs = new FileStream(_cfgFilePath, FileMode.Create)) {
+                        fs.Write(res);
+                        fs.Close();
+                    }
                 }
+
+                if (!File.Exists(_cfgFilePath)) throw new ArgumentException($@"Configuration file {_cfgFileName} doesn't exists. Please add conf file.");
+                var txt = File.ReadAllText(_cfgFilePath);
+               _cfg = JsonNode.Parse(txt);
+            } catch (Exception ex) {
+                _logger?.LogError(ex.Message);
             }
-
-
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
@@ -29,7 +41,10 @@ namespace ddns_hcli {
                 if (_logger.IsEnabled(LogLevel.Information)) {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
-                await Task.Delay(1000, stoppingToken); 
+
+               
+
+                await Task.Delay(5000, stoppingToken); // Check every 5 seconds.
             }
         }
     }
