7fd852627284fd9916fecfd3290cb48f1bd3fec1
Lingam <rmsmech@gmail.com>
2024-11-20 09:45:24 +0400

    Updated log information

--- ddns-hcli/Program.cs
+++ ddns-hcli/Program.cs
@@ -18,7 +18,7 @@ if (!args.Contains("-v", StringComparer.OrdinalIgnoreCase)) {
   
     //Clear other providers first.
     builder.Logging.ClearProviders(); //Don't log to console
-    builder.Logging.AddHaleyFileLogger((o) => { o.OutputDirectory = logdir; });
+    builder.Logging.AddHaleyFileLogger((o) => { o.OutputDirectory = logdir; o.FileName = "ddns-hcli"; }); //Since this is a worker service, the log will be created one time and rotation will not happen. So, ensure that single file name is used and rotation is done outside or, conduct log rotation outside of the logger.
 }
 builder.Services.AddHostedService<Worker>();
 
