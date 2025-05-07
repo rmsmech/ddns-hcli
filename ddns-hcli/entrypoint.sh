61f04ab14224110ed88d09cfb91230f77644d8b4
Quetzal <rmsmech@outlook.com>
2025-05-07 23:20:34 +0400

    cleaning up the script files

---
+++ ddns-hcli/entrypoint.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/sh 
+# We use above line becuase we deploy this in alpine linux where bin/bash is not present. 
+
+set -e  # Exit immediately if a command fails
+
+echo "Copying the podman systemd service file.."
+
+if [ ! -f "/usr/share/hdns/hdns.service" ]; then
+    cp /app/hdns/hdns.service /usr/share/hdns/hdns.service
+fi
+
+echo "Copying the logrotate file.."
+
+if [ ! -f "/usr/share/hdns/hlog" ]; then
+    cp /app/hdns/hlog /usr/share/hdns/hlog
+fi
+
+echo "Starting HDNS application..."
+
+# Execute .NET application
+exec dotnet /app/hdns/HDNS.dll
